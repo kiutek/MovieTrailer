c7d577dad5b3f04047a4da0e8052d01c19dcb463
Marcel Eric Loiselle <marceleric@kiutek.com>
2016-07-29 15:51:15 -0400

    Added more comment

--- media.py
+++ media.py
@@ -5,6 +5,9 @@ class Movie():
     The Movie class hold the information related to the movies to be displayed
 """
     def __init__(self, movie_title, movie_storyline, poster_image,
+    """
+        Initialize the instance property
+    """
                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
@@ -12,4 +15,7 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+    """
+        Open the trailer URL
+    """
         webbrowser.open(self.trailer_youtube_url)
