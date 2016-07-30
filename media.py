f8560f1a7633927e1acc83355ad489ca99219c4a
Marcel Eric Loiselle <marceleric@kiutek.com>
2016-07-30 07:13:40 -0400

    Fix alignment issue with docstring

--- media.py
+++ media.py
@@ -1,21 +1,21 @@
 import webbrowser
 
 class Movie():
-"""
-    The Movie class hold the information related to the movies to be displayed
-"""
+    """
+        The Movie class hold the information related to the movies to be displayed
+    """
     def __init__(self, movie_title, movie_storyline, poster_image,
-    """
-        Initialize the instance property
-    """
-                trailer_youtube):
+                    trailer_youtube):
+        """
+            Initialize the instance property
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-    """
-        Open the trailer URL
-    """
+        """
+            Open the trailer URL
+        """
         webbrowser.open(self.trailer_youtube_url)
